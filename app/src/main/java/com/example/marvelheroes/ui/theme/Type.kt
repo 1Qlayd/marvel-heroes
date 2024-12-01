91df266ac6f6eb124da09ba9283916d7507c825a
1Qlayd <kostya.pashkov.90@mail.ru>
2024-12-02 00:36:04 +0600

    lab 2

--- app/src/main/java/com/example/marvelheroes/ui/theme/Type.kt
+++ app/src/main/java/com/example/marvelheroes/ui/theme/Type.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     headlineLarge = TextStyle(
         fontSize = 32.sp,
